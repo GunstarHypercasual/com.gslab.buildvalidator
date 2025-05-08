9973050799fab019a982a7a9ed0fef2ba2b80c22
Dieptt88 <dieptt88@gmail.com>
2025-05-08 15:44:44 +0700

    * Make it complete

--- GSLab/Editor/BuildValidatorWindow.cs
+++ GSLab/Editor/BuildValidatorWindow.cs
@@ -83,12 +83,12 @@ namespace GSLab.BuildValidator
             
             GUI.backgroundColor = prevBG;
             
-            if (GUILayout.Button("Generate CSV Only"))
+            if (GUILayout.Button("DEBUG - Generate CSV Only"))
             {
-                CreateCSVFile();
+                listingManager.GenerateCsv();
             }
 
-            if (GUILayout.Button("Generate material folder"))
+            if (GUILayout.Button("DEBUG - Generate material folder"))
             {
                 listingManager.PrepareDirectory();
                 listingManager.ValidateFiles(false);
@@ -98,89 +98,7 @@ namespace GSLab.BuildValidator
             GUILayout.Space(30);
             DrawStepStatus();
         }
-
-        void CreateCSVFile()
-        {
-#if UNIPAY_PRESENT
-            Debug.Log("Generating CSV file...");
-            var asset = IAPScriptableObject.GetOrCreateSettings();
-            int idx = settings.IAPCategoryIndex;
-            Debug.Log($"Category index: {idx}");
-            if (idx <= 0 || idx >= asset.categoryList.Count)
-            {
-                Debug.LogError("Invalid category index for CSV generation.");
-                return;
-            }
-            
-            var category = asset.categoryList[idx - 1];
-            Debug.Log($"Category: {category.ID}");
-            var items = category.ID;
-
-            var allProducts = asset.productList;
-            Debug.Log($"All products: {allProducts.Count}");
-            var filtered = allProducts.Where(p => p.category.ID == category.ID && p.type == ProductType.Consumable).ToList();
-            if (filtered.Count == 0)
-            {
-                Debug.LogWarning($"No products found in category '{category.ID}'.");
-                return;
-            }
-
-            var csvName = $"{PlayerSettings.applicationIdentifier}_iap";
-            var ext = ".csv";
-            var listCountry = new List<string>();
-            listCountry.Add("US");
-            listCountry.Add("VN");
-            foreach (var country in listCountry)
-            {
-                var csvPath = Path.Combine(Application.dataPath, $"{csvName}_{country}{ext}");
-                Debug.Log($"CSV path: {csvPath}");
-                using (var writer = new StreamWriter(csvPath, false, new UTF8Encoding(false)))
-                {
-                    Debug.Log("Start write csvs");
-                    writer.WriteLine("Product ID,Published State,Purchase Type,Auto Translate,Locale; Title; Description,Auto Fill Prices,Price,Pricing Template ID,EEA Withdrawal Right Type,Reduced VAT Rates,Communications and amusement taxes,Tokenized digital asset declared");
-                    foreach (var p in filtered)
-                    {
-                        var price = p.GetPriceString();
-                        var priceClean = price.Replace(" ", "").Replace("$", "");
-                        var priceDecimal = Convert.ToDecimal(priceClean);
-                        var multiplePriceDecimal = priceDecimal * 1000000m;
-                        var finalPrice = multiplePriceDecimal;
-                        var priceField = $"{finalPrice}";
-
-                        if (country == "VN")
-                        {
-                            if (!decimal.TryParse(priceClean, NumberStyles.AllowLeadingSign | NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture,
-                                    out var priceCleanDec))
-                            {
-                                Debug.LogWarning($"Invalid price: {priceClean}");
-                                priceCleanDec = 0m;
-                            }
-
-                            const decimal usdToVNDRate = 25200m;
-                            finalPrice = priceCleanDec * usdToVNDRate * 1000000m;
-                            // Debug.Log($"finalPrice : {finalPrice}");
-                            priceField = $"{Math.Round(finalPrice, 2).ToString("F0", CultureInfo.InvariantCulture)}";
-                            Debug.Log($"vndString : {priceField}");
-                            // priceString = vndString;
-                            // Debug.Log($"Price: {priceString}");
-                        }
-                        
-                        Debug.Log($"finalPrice : {finalPrice}");
-                        
-                        string id = p.ID;
-                        string t = p.title;
-                        string desc = p.description;
-                        Debug.Log($"Product ID: {id}, Title: {t}, Description: {desc}");
-                    
-                        writer.WriteLine($"{id},published,managed_by_android,false,en-US; {t}; {desc},true,{priceField},,DIGITAL_CONTENT,,,false");
-                    }
-                }
-            }
-#else
-        Debug.LogError("UNIPAY_PRESENT does not exist in defines symbol");
-#endif
-        }
-
+        
         void DrawStepStatus()
         {
             if (stepStatus == null || stepStatus.Count == 0) return;
--- GSLab/Editor/StoreListingManager.cs
+++ GSLab/Editor/StoreListingManager.cs
@@ -140,11 +140,89 @@ namespace GSLab.BuildValidator
             return true;
         }
 
-        bool GenerateCsv()
+        public bool GenerateCsv()
         {
-            // File.WriteAllText(Path.Combine(dir, "iap_products.csv"), "id,price,description");
-            // return true;
-            return false;
+#if UNIPAY_PRESENT
+            Debug.Log("Generating CSV file...");
+            var asset = IAPScriptableObject.GetOrCreateSettings();
+            int idx = settings.IAPCategoryIndex;
+            Debug.Log($"Category index: {idx}");
+            if (idx <= 0 || idx >= asset.categoryList.Count)
+            {
+                Debug.LogError("Invalid category index for CSV generation.");
+                return false;
+            }
+            
+            var category = asset.categoryList[idx - 1];
+            Debug.Log($"Category: {category.ID}");
+            var items = category.ID;
+
+            var allProducts = asset.productList;
+            Debug.Log($"All products: {allProducts.Count}");
+            var filtered = allProducts.Where(p => p.category.ID == category.ID && p.type == ProductType.Consumable).ToList();
+            if (filtered.Count == 0)
+            {
+                Debug.LogWarning($"No products found in category '{category.ID}'.");
+                return false;
+            }
+
+            var csvName = $"{PlayerSettings.applicationIdentifier}_iap";
+            var ext = ".csv";
+            var listCountry = new List<string>();
+            listCountry.Add("US");
+            listCountry.Add("VN");
+            foreach (var country in listCountry)
+            {
+                var csvPath = Path.Combine(Application.dataPath, $"{csvName}_{country}{ext}");
+                Debug.Log($"CSV path: {csvPath}");
+                using (var writer = new StreamWriter(csvPath, false, new UTF8Encoding(false)))
+                {
+                    Debug.Log("Start write csvs");
+                    writer.WriteLine("Product ID,Published State,Purchase Type,Auto Translate,Locale; Title; Description,Auto Fill Prices,Price,Pricing Template ID,EEA Withdrawal Right Type,Reduced VAT Rates,Communications and amusement taxes,Tokenized digital asset declared");
+                    foreach (var p in filtered)
+                    {
+                        var price = p.GetPriceString();
+                        var priceClean = price.Replace(" ", "").Replace("$", "");
+                        var priceDecimal = Convert.ToDecimal(priceClean);
+                        var multiplePriceDecimal = priceDecimal * 1000000m;
+                        var finalPrice = multiplePriceDecimal;
+                        var priceField = $"{finalPrice}";
+
+                        if (country == "VN")
+                        {
+                            if (!decimal.TryParse(priceClean, NumberStyles.AllowLeadingSign | NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture,
+                                    out var priceCleanDec))
+                            {
+                                Debug.LogWarning($"Invalid price: {priceClean}");
+                                priceCleanDec = 0m;
+                            }
+
+                            const decimal usdToVNDRate = 25200m;
+                            finalPrice = priceCleanDec * usdToVNDRate * 1000000m;
+                            // Debug.Log($"finalPrice : {finalPrice}");
+                            priceField = $"{Math.Round(finalPrice, 2).ToString("F0", CultureInfo.InvariantCulture)}";
+                            Debug.Log($"vndString : {priceField}");
+                            // priceString = vndString;
+                            // Debug.Log($"Price: {priceString}");
+                        }
+                        
+                        Debug.Log($"finalPrice : {finalPrice}");
+                        
+                        string id = p.ID;
+                        string t = p.title;
+                        string desc = p.description;
+                        Debug.Log($"Product ID: {id}, Title: {t}, Description: {desc}");
+                    
+                        writer.WriteLine($"{id},published,managed_by_android,false,en-US; {t}; {desc},true,{priceField},,DIGITAL_CONTENT,,,false");
+                    }
+                }
+            }
+            File.Copy(csvPath, Path.Combine(dir, $"{csvName}{ext}"), true);
+#else
+        Debug.LogError("UNIPAY_PRESENT does not exist in defines symbol");
+#endif
+            
+            return true;
         }
 
         public bool RevealDirectory() { EditorUtility.RevealInFinder(dir); return true; }
